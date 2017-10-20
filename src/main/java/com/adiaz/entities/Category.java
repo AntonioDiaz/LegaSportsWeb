aab6f2aa1e3b22b30ae3df092e6d9fae31f2c243
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2017-10-20 22:10:04 +0200

    Add lombok support in Category

--- src/main/java/com/adiaz/entities/Category.java
+++ src/main/java/com/adiaz/entities/Category.java
@@ -5,9 +5,11 @@ import java.io.Serializable;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.*;
+import lombok.Data;
 
 @Entity
 @Cache
+@Data
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -20,29 +22,4 @@ public class Category implements Serializable {
 	
 	@Index
 	private Integer order;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getOrder() {
-		return order;
-	}
-
-	public void setOrder(Integer order) {
-		this.order = order;
-	}
-
 }
