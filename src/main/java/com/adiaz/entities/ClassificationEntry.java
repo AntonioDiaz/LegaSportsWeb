6f971f07c9162ec19ecc722605bdd09b23937b92
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2017-10-23 16:19:56 +0200

    adding lombok support to ClassificationEntry

--- src/main/java/com/adiaz/entities/ClassificationEntry.java
+++ src/main/java/com/adiaz/entities/ClassificationEntry.java
@@ -3,11 +3,13 @@ package com.adiaz.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.*;
+import lombok.Data;
 
 import java.io.Serializable;
 
 @Entity
 @Cache
+@Data
 public class ClassificationEntry  implements Serializable {
 
 	@Id
@@ -61,123 +63,4 @@ public class ClassificationEntry  implements Serializable {
 		return points - sanctions;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Ref<Competition> getCompetitionRef() {
-		return competitionRef;
-	}
-
-	public void setCompetitionRef(Ref<Competition> competition) {
-		this.competitionRef = competition;
-	}
-
-	public Integer getPosition() {
-		return position;
-	}
-
-	public void setPosition(Integer position) {
-		this.position = position;
-	}
-
-	public Integer getPoints() {
-		return points;
-	}
-
-	public void setPoints(Integer points) {
-		this.points = points;
-	}
-
-	public Integer getMatchesPlayed() {
-		return matchesPlayed;
-	}
-
-	public void setMatchesPlayed(Integer matchesPlayed) {
-		this.matchesPlayed = matchesPlayed;
-	}
-
-	public Integer getMatchesWon() {
-		return matchesWon;
-	}
-
-	public void setMatchesWon(Integer matchesWon) {
-		this.matchesWon = matchesWon;
-	}
-
-	public Integer getMatchesDrawn() {
-		return matchesDrawn;
-	}
-
-	public void setMatchesDrawn(Integer matchesDrawn) {
-		this.matchesDrawn = matchesDrawn;
-	}
-
-	public Integer getMatchesLost() {
-		return matchesLost;
-	}
-
-	public void setMatchesLost(Integer matchesLost) {
-		this.matchesLost = matchesLost;
-	}
-
-	public Ref<Team> getTeamRef() {
-		return teamRef;
-	}
-
-	public void setTeamRef(Ref<Team> teamRef) {
-		this.teamRef = teamRef;
-	}
-
-	public Team getTeamEntity() {
-		return teamEntity;
-	}
-
-	public void setTeamEntity(Team teamEntity) {
-		this.teamEntity = teamEntity;
-	}
-
-	public String getTeam() {
-		return team;
-	}
-
-	public void setTeam(String team) {
-		this.team = team;
-	}
-
-	public Competition getCompetition() {
-		return competition;
-	}
-
-	public void setCompetition(Competition competition) {
-		this.competition = competition;
-	}
-
-	public Integer getGoalsFor() {
-		return goalsFor;
-	}
-
-	public void setGoalsFor(Integer goalsFor) {
-		this.goalsFor = goalsFor;
-	}
-
-	public Integer getGoalsAgainst() {
-		return goalsAgainst;
-	}
-
-	public void setGoalsAgainst(Integer goalsAgainst) {
-		this.goalsAgainst = goalsAgainst;
-	}
-
-	public Integer getSanctions() {
-		return sanctions;
-	}
-
-	public void setSanctions(Integer sanctions) {
-		this.sanctions = sanctions;
-	}
 }
