b0244297a71a1f27cc05c599cf35687ef0653ca6
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2017-10-24 17:14:21 +0200

    add Lombok support to Club entity

--- src/main/java/com/adiaz/entities/Club.java
+++ src/main/java/com/adiaz/entities/Club.java
@@ -3,6 +3,7 @@ package com.adiaz.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.*;
+import lombok.Data;
 
 import java.io.Serializable;
 
@@ -11,6 +12,7 @@ import java.io.Serializable;
  */
 @Entity
 @Cache
+@Data
 public class Club  implements Serializable {
 	@Id
 	private Long id;
@@ -39,70 +41,5 @@ public class Club  implements Serializable {
 		if (townRef!=null && townRef.isLoaded()){
 			townEntity = townRef.get();
 		}
-	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getContactPerson() {
-		return contactPerson;
-	}
-
-	public void setContactPerson(String contactPerson) {
-		this.contactPerson = contactPerson;
-	}
-
-	public String getContactEmail() {
-		return contactEmail;
-	}
-
-	public void setContactEmail(String contactEmail) {
-		this.contactEmail = contactEmail;
-	}
-
-	public String getContactAddress() {
-		return contactAddress;
-	}
-
-	public void setContactAddress(String contactAddress) {
-		this.contactAddress = contactAddress;
-	}
-
-	public String getContactPhone() {
-		return contactPhone;
-	}
-
-	public void setContactPhone(String contactPhone) {
-		this.contactPhone = contactPhone;
-	}
-
-	public Ref<Town> getTownRef() {
-		return townRef;
-	}
-
-	public void setTownRef(Ref<Town> townRef) {
-		this.townRef = townRef;
-	}
-
-	public Town getTownEntity() {
-		return townEntity;
-	}
-
-	public void setTownEntity(Town townEntity) {
-		this.townEntity = townEntity;
 	}
 }
